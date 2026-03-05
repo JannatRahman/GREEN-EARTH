7f64bf810e8f740ff34d5423b1373c03ad03390b
JannatRahman <you@example.com>
2026-03-05 19:04:53 +0600

    button added using DOM

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const categoriesContainer = document.getElementById("categoriesContainer");
+
+async function loadCategories() {
+  // async await
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/categories"
+  );
+  const data = await res.json();
+  console.log(data);
+  console.log(categoriesContainer);
+  data.categories.forEach((category) => {
+    console.log(category);
+    const btn = document.createElement("button");
+    btn.className = "btn btn-outline w-full";
+    btn.textContent = category.category_name
+    categoriesContainer.appendChild(btn);
+
+  });
+}
+loadCategories();
