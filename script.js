b2cd720b32b2b1f08868a05d77ea62e43b394741
JannatRahman <you@example.com>
2026-03-06 16:07:44 +0600

    adjustments made

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const categoriesContainer = document.getElementById("categoriesContainer");
+const treesContainer= document.getElementById("tressContainer");
 
 async function loadCategories() {
   // async await
@@ -7,10 +8,10 @@ async function loadCategories() {
     "https://openapi.programming-hero.com/api/categories"
   );
   const data = await res.json();
-  console.log(data);
-  console.log(categoriesContainer);
+  // console.log(data);
+  // console.log(categoriesContainer);
   data.categories.forEach((category) => {
-    console.log(category);
+    // console.log(category);
     const btn = document.createElement("button");
     btn.className = "btn btn-outline w-full";
     btn.textContent = category.category_name
@@ -18,4 +19,46 @@ async function loadCategories() {
 
   });
 }
+async function loadTress() {
+  const res = await fetch("https://openapi.programming-hero.com/api/plants");
+  const data = await res.json();
+  displayTrees(data.plants);
+}
+
+function displayTrees(trees){
+  console.log(trees);
+  trees.forEach((tree) => {
+    console.log(tree);
+    const card = document.createElement("div")
+    card.className = "card bg-white shadow-sm"
+    card.innerHTML = `<figure>
+      <img
+      src="${tree.image}"
+      alt="${tree.name}" 
+      title="${tree.name}"
+      class="h-48 w-full object-cover"
+      />S
+ </figure>
+
+       <div class="card-body">
+    <h2 class="card-title">${tree.name}</h2>
+    <p class="line-clamp-2">
+    A card component has a figure, a body part, and inside body there are title and actions parts
+    </p>
+
+     <div class="badge badge-success badge-outline">${tree.category}</div>
+
+    <div class="flex justify-between items-center gap-5">
+      <h2 class="font-bold text-xl text-[#4ade80]">
+        $${tree.price}
+      </h2>
+      <button class="btn btn-primary">Cart</button>
+      </div>
+     </div>`;
+        treesContainer.appendChild(card);
+  })
+}
+
+
 loadCategories();
+loadTress();
